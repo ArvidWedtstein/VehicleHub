b8b139c86a42353e253f16a57b13728a0d0c7587
ArvidWedtstein <71834553+ArvidWedtstein@users.noreply.github.com>
2026-02-11 21:48:01 +0100

    removed unused code

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -85,29 +85,6 @@ definePageMeta({
             of maintenance with ease. Welcome to your personal Vehicle Hub.
           </p>
         </div>
-        <div class="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
-          <div
-            class="grid grid-cols-1 gap-x-8 gap-y-6 text-base/7 font-semibold text-white sm:grid-cols-2 md:flex lg:gap-x-10"
-          >
-            <!-- <a v-for="link in links" :key="link.name" :href="link.href"
-              >{{ link.name }} <span aria-hidden="true">&rarr;</span></a
-            > -->
-          </div>
-          <dl
-            class="mt-16 grid grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 lg:grid-cols-4"
-          >
-            <!-- <div
-              v-for="stat in stats"
-              :key="stat.name"
-              class="flex flex-col-reverse gap-1"
-            >
-              <dt class="text-base/7 text-gray-300">{{ stat.name }}</dt>
-              <dd class="text-4xl font-semibold tracking-tight text-white">
-                {{ stat.value }}
-              </dd>
-            </div> -->
-          </dl>
-        </div>
       </div>
     </div>
   </div>
