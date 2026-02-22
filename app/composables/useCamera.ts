3c6470786fc1e2baa84a1d83cad259a7621a9a72
ArvidWedtstein <71834553+ArvidWedtstein@users.noreply.github.com>
2026-02-22 14:45:45 +0100

    removed unused var

--- app/composables/useCamera.ts
+++ app/composables/useCamera.ts
@@ -87,7 +87,7 @@ export const useCamera = (
     const ctx = canvas.getContext("2d");
     if (!ctx) return;
 
-    const { videoWidth, videoHeight, width, height } = video;
+    const { videoWidth, videoHeight } = video;
 
     canvas.width = videoWidth;
     canvas.height = videoHeight;
