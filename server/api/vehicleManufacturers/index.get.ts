b02aa29c04099f9182ccee24a2aa8a2e359df7b4
ArvidWedtstein <71834553+ArvidWedtstein@users.noreply.github.com>
2026-03-15 15:25:33 +0100

    Refactor event handlers to use authenticated and cache vehicle manufacturers

--- server/api/vehicleManufacturers/index.get.ts
+++ server/api/vehicleManufacturers/index.get.ts
@@ -1,7 +1,7 @@
 import { serverSupabaseClient } from "#supabase/server";
 import { Database } from "~/types/supabase";
 
-export default defineEventHandler(async (event) => {
+export default defineCachedEventHandler(async (event) => {
   const client = await serverSupabaseClient<Database>(event);
 
   const { data, error } = await client
