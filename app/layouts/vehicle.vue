c5e7edf8c8f144fa75203e2ea64214c0eeb524d0
ArvidWedtstein <71834553+ArvidWedtstein@users.noreply.github.com>
2026-05-13 17:13:47 +0200

    fix: add vehicleId check before fetching service insights

--- app/layouts/vehicle.vue
+++ app/layouts/vehicle.vue
@@ -96,7 +96,7 @@ onMounted(() => {
 
 <template>
   <div
-    class="relative flex flex-col gap-4 flex-1 w-full p-4 min-h-0 h-[calc(100dvh-calc(var(--ui-header-height)*2))] lg:h-[calc(100dvh-var(--ui-header-height))] overflow-auto"
+    class="relative flex flex-col gap-4 flex-1 w-full lg:p-4 min-h-0 h-[calc(100dvh-calc(var(--ui-header-height)*2))] lg:h-[calc(100dvh-var(--ui-header-height))] overflow-auto"
   >
     <NuxtRouteAnnouncer />
     <NuxtAnnouncer />
