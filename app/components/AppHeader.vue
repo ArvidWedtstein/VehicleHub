f348059d605ce078863cb252169b2087d1c00843
ArvidWedtstein <71834553+ArvidWedtstein@users.noreply.github.com>
2026-05-14 17:01:30 +0200

    fix: correct user ID reference in profile link

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -47,7 +47,7 @@ const menuItems = computed<DropdownMenuItem[]>(() => {
       {
         label: "Profile",
         icon: "mdi:account",
-        to: `/profiles/${user.value.user_metadata.id}`,
+        to: `/profiles/${user.value.id}`,
       },
       {
         label: "Logout",
