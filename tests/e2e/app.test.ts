03c93b798d9688c02fc26ce1763f6a1ed9dc70ef
ArvidWedtstein <71834553+ArvidWedtstein@users.noreply.github.com>
2026-06-10 22:29:16 +0200

    feat: integrate Playwright for enhanced end-to-end testing

--- tests/e2e/app.test.ts
+++ tests/e2e/app.test.ts
@@ -1,5 +1,9 @@
 import { setup, createPage, $fetch, url } from "@nuxt/test-utils/e2e";
 import { describe, it, test, expect } from "vitest";
+import {
+  test as playwrightTest,
+  expect as playwrightExpect,
+} from "@nuxt/test-utils/playwright";
 
 describe("Vehicle Hub App", async () => {
   await setup();
@@ -12,5 +16,10 @@ describe("Vehicle Hub App", async () => {
   test("with playwright", async () => {
     const page = await createPage();
     await page.goto(url("/"), { waitUntil: "hydration" });
+  });
+
+  playwrightTest("test", async ({ page, goto }) => {
+    await goto("/", { waitUntil: "hydration" });
+    await playwrightExpect(page.getByRole("heading")).toHaveText("Vehicle");
   });
 });
